5ef18c6a1fb8719d719b2445d039d8e26c5331ec
Tommaso Sartor <tmmsartor@gmail.com>
2019-09-10 16:45:20 +0200

    Update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,20 @@
 #! /bin/bash
 
 pushd libraries
-git clone   https://github.com/tmmsartor/acados.git
+git clone https://github.com/tmmsartor/acados.git
 
 pushd acados
-git submodule update --recursive --init
+git submodule update --init external/hpipm
+git submodule update --init external/blasfeo
+git submodule update --init external/qpoases
+
 git checkout teensy_platform
-echo "$(pwd)/../../" > Makefile.local
+echo "TEMPLATES_PATH = $(pwd)/../../" > Makefile.local
 make -j4
 popd
 
-ln -s acados/hpipm .
-ln -s acados/blasfeo .
+ln -s acados/external/hpipm .
+ln -s acados/external/blasfeo .
 popd
 
 make -j4
